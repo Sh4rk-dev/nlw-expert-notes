a550575a9f2545b41dab52aed9ab01286f60738f
Renan <renanrapace13@gmail.com>
2024-02-10 20:13:06 -0300

    Starting the Creation of Notes Components with ReactJS

---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import logo from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/newNoteCard";
+import { NoteCard } from "./components/noteCard";
+
+export function App() {
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
+  );
+}
