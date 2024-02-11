16cd1a5faf44684d8c18ffd840c739011e6acc69
Renan <renanrapace13@gmail.com>
2024-02-11 01:53:41 -0300

    Creating the modal

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import logo from "./assets/logo-nlw-expert.svg";
-import { NewNoteCard } from "./components/newNoteCard";
-import { NoteCard } from "./components/noteCard";
+import { NewNoteCard } from "./components/new-note-card";
+import { NoteCard } from "./components/note-card";
 
 export function App() {
   return (
@@ -18,9 +18,12 @@ export function App() {
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: "Hello word",
+          }}
+        />
       </div>
     </div>
   );
